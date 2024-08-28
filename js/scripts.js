334adc88b819e791b256497fe3e3c5ed450ca824
EliaPennoni <elia.pennoni1991@gmail.com>
2024-08-28 19:44:33 +0200

    esercizio terminato

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 console.log('js connected');
+
+const { createApp, ref } = Vue;
+
+createApp({
+    data() {
+      // Definizione dei dati reattivi
+      const message = ref('Hello world!');
+      const imgUrl = ref('img/IMG_4275.jpg');
+      const imageClass = ref('medium-img'); 
+      return {
+        message,
+        imgUrl,
+        imageClass
+      };
+    }
+}).mount('#app');
