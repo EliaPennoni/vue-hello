d6368098d7a2449ffd9b2fd3e8a38ddc4be950ba
EliaPennoni <elia.pennoni1991@gmail.com>
2024-08-29 14:26:27 +0200

    modifiche finali apportate

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 console.log('js connected');
 
-const { createApp, ref } = Vue;
+const { createApp } = Vue;
 
-createApp({
-    data() {
-      // Definizione dei dati reattivi
-      const message = ref('Hello world!');
-      const imgUrl = ref('img/IMG_4275.jpg');
-      const imageClass = ref('medium-img'); 
-      return {
-        message,
-        imgUrl,
-        imageClass
-      };
-    }
-}).mount('#app');
+const app = createApp({
+  data() {
+    return {
+      message :('Hello world!'),
+      imgUrl :('img/IMG_4275.jpg')
+    };
+  }
+});
+
+app.mount('#app');
